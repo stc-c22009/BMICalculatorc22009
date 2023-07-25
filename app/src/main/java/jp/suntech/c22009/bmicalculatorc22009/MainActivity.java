c8dd70b8f514e0f068d978474ff602f65a5c6489
stc-c22009 <c22009@suntech.jp>
2023-07-25 12:05:02 +0900

    Initial commit

---
+++ app/src/main/java/jp/suntech/c22009/bmicalculatorc22009/MainActivity.java
@@ -0,0 +1,14 @@
+package jp.suntech.c22009.bmicalculatorc22009;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
